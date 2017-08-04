192ce22f6d8d1dbe17001dc808583f495ab3e714
Timothy Adamson <MrTimcakes@gmail.com>
2017-08-04 23:03:43 +0100

    Add MDC Theme and React Dev Tools

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './style';
+import 'preact-material-components/Typography/style.css';
+import 'preact-material-components/Theme/style.css';
 import App from './components/app';
+require('preact/devtools');
 
 export default App;
