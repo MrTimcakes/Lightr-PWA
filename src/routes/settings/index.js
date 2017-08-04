b1cb3b987222e29d7bedaed1c1bc5e5f99580d01
Timothy Adamson <MrTimcakes@gmail.com>
2017-08-04 23:06:14 +0100

    Temporary Settings Page

---
+++ src/routes/settings/index.js
@@ -0,0 +1,13 @@
+import { h, Component } from 'preact';
+import style from './style';
+
+export default class Settings extends Component {
+	render() {
+		return (
+			<div class={style.settings}>
+				<h1>Settings</h1>
+				<p>This is the Settings component.</p>
+			</div>
+		);
+	}
+}
