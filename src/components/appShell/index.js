2882ca1b9e8019b1eb3c91aa53f10d3ecc7d7ee1
Timothy Adamson <MrTimcakes@gmail.com>
2017-08-04 23:07:39 +0100

    Add App Shell Component

---
+++ src/components/appShell/index.js
@@ -0,0 +1,50 @@
+import { h, Component } from 'preact';
+import { route } from 'preact-router';
+import Toolbar from 'preact-material-components/Toolbar';
+import Drawer from 'preact-material-components/Drawer';
+import List from 'preact-material-components/List';
+import style from './style';
+
+export default class AppShell extends Component {
+
+	closeDrawer(){
+		this.drawer.MDComponent.open = false;
+	}
+
+	render() {
+		return (
+			<div>
+				<Toolbar>
+					<Toolbar.Row>
+						<Toolbar.Section align-start={true}>
+							<Toolbar.Icon href="#" onClick={(e)=>{ e.preventDefault(); this.drawer.MDComponent.open = true; }}>
+								menu
+							</Toolbar.Icon>
+							<Toolbar.Title>
+								Lightr
+							</Toolbar.Title>
+						</Toolbar.Section>
+					</Toolbar.Row>
+				</Toolbar>
+				<Drawer.TemporaryDrawer ref={drawer=>{this.drawer = drawer;}} >
+	        <Drawer.TemporaryDrawerHeader className="mdc-theme--primary-bg">
+	          Lightr
+	        </Drawer.TemporaryDrawerHeader>
+	        <Drawer.TemporaryDrawerContent>
+	        <List>
+	          <List.LinkItem onClick={(e) =>{ route("/"); this.closeDrawer(); }}>
+	            <List.ItemIcon>home</List.ItemIcon>
+	            Home
+	          </List.LinkItem>
+						<hr class="mdc-list-divider" />
+	          <List.LinkItem onClick={(e) =>{ route("/settings"); this.closeDrawer(); }}>
+	            <List.ItemIcon>settings</List.ItemIcon>
+	            Settings
+	          </List.LinkItem>
+	        </List>
+	        </Drawer.TemporaryDrawerContent>
+	      </Drawer.TemporaryDrawer>
+			</div>
+		);
+	}
+}
