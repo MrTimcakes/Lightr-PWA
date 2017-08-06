94e5aba4d431768f95571fa7159568980baf55f1
Timothy Adamson <MrTimcakes@gmail.com>
2017-08-06 10:13:18 +0100

    Switch To Static HMTL Instead Of Preact
    React development temporarily moved to separate branch. Master branch
    will now be done with normal HMTL, JS and SCSS

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './style';
-import 'preact-material-components/Typography/style.css';
-import 'preact-material-components/Theme/style.css';
-import App from './components/app';
-require('preact/devtools');
-
-export default App;
