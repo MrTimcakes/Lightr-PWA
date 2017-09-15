8be990b07c4968b4fd89d5320a8fa19cf9fcca5f
Timothy Adamson <MrTimcakes@gmail.com>
2017-09-16 00:30:39 +0100

    Basic Fully Working System

--- src/components/lightrController/index.js
+++ src/components/lightrController/index.js
@@ -8,14 +8,15 @@ import style from './style';
 export default class lightrController extends Component {
   constructor() {
     super();
-    this.state.settings = {};
+    this.state.devices = [];
   }
 
   componentDidMount() {
     var that = this;
-    localforage.getItem('MQTTSETTINGS').then(function(value) {
-      if(value!=null){
-				MQTTConnect(value);
+    window.lightrController = this;
+    localforage.getItem('MQTTSETTINGS').then(function(settings) {
+      if(settings!=null){
+				MQTTConnect(settings);
 			}else{
 				//Not yet set
 			}
@@ -28,10 +29,18 @@ export default class lightrController extends Component {
     MQTTdisconnect();
   }
 
+
+
   render(props, state) {
     return (
 			<div class={ style.lightrController }>
-				boi
+        <ul>
+          {
+            Object.keys(this.state.devices).map(function(key) {
+              return <li onClick={()=>{toggleLight(this.state.devices[key])}}>{this.state.devices[key]['nickname']} {this.state.devices[key]['status']?"On":"Off"}</li>
+            }.bind(this))
+          }
+        </ul>
 			</div>
 		);
   }
@@ -74,10 +83,33 @@ function onConnectionLost(responseObject) {
 }
 
 function onMessageArrived(message) {
-  console.log("Message: " + message.payloadString);
+  console.log(message);
+  if(message.destinationName === "lights/all/status"){
+    let newDevice = JSON.parse(message.payloadString);
+    let tempDevices = window.lightrController.state.devices;
+
+    let alreadyExists = tempDevices.find((o, i) => {
+      if (o.MAC === newDevice.MAC) {
+          tempDevices[i] = newDevice;
+          return true;
+      }
+    });
+    if(!alreadyExists){
+      tempDevices.push(newDevice);
+    }
+
+    window.lightrController.setState({devices: tempDevices});
+  }
 }
 
 function MQTTdisconnect(){
   window.MQTTClient.disconnect();
   console.log("Disconnected");
+}
+
+function toggleLight(light){
+  console.log(light.MAC);
+  let message = new Paho.MQTT.Message("t");
+  message.destinationName = `lights/${light.MAC}/set`;
+  window.MQTTClient.send(message);
 }
