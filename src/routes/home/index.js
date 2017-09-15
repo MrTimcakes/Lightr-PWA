aaddf6a72e9f3822c780f5d10b969cb2b829fb35
Timothy Adamson <MrTimcakes@gmail.com>
2017-09-15 14:56:20 +0100

    Use LightrController

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -7,8 +7,6 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home route.</p>
 				<LightrController />
 			</div>
 		);
